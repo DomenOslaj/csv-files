2581ab2e53636c080aa1dc03fae2fe239a8ab1a0
Domen Oslaj <doslaj@gmail.com>
2020-10-03 12:11:08 +0200

    added with open, splitlines, for loop, row.split

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+with open("person.csv", "r") as person_file:
+    persons = person_file.read()
 
+persons = persons.splitlines()
+
+print(persons)
+
+for row in persons:
+    person = row.split(", ")
+    print(person)
