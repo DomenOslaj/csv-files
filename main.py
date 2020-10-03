b86ce72bea4c0b9e8f62e784b33525f1c362049d
Domen Oslaj <doslaj@gmail.com>
2020-10-03 13:44:15 +0200

    row.split-space next to comma removed, list index print added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 with open("person.csv", "r") as person_file:
     persons = person_file.read()
 
-persons = persons.splitlines()
+persons = persons.splitlines()     #split a string into a list where each line is a list item
 
 print(persons)
 
 for row in persons:
-    person = row.split(", ")
-    print(person)
+    person = row.split(",")  #split each list item to more words, each word is a list item
+    print("{0} is {1} years old and {2}." .format(person[0], person[1], person[2]))
+
